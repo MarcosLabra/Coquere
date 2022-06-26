0f29fc436614ab23423b039d3ae7f4ea4a793521
Pablo <delatorre.pablodaniel@gmail.com>
2022-06-26 20:07:10 -0300

    informacion y estructura general del footer hechas, falta agregar fuentes, proporciones, estilos y distancias a los componentes del footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,30 +1,34 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            <div>
+            <div className="coquere">
                 <h2>Coquere escuela de cocina</h2>
                 <a href=""></a>
                 <a href=""></a>
                 <a href=""></a>
             </div>
-            <div>
+            <div className="instalaciones">
                 <h2>Nuestras instalaciones</h2>
                 <p>Direccion</p>
                 <p>Complemento direccion</p>
             </div>
-            <div>
+            <div className="contacto">
                 <h2>Contacto</h2>
                 <p>Teléfono</p>
                 <p>Celular</p>
                 <p>Correo</p>
             </div>
-            <div>
+            <div className="redes">
                 <h2>Redes sociales</h2>
                 <a href="">Facebook</a>
                 <a href="">Instagram</a>
             </div>
-            <div></div>
-            <div></div>
+            <div className="logo">
+                <img src="/images/branding/CoquereLogoCircularFooter.webp" alt="Coquere logo footer" />
+            </div>
+            <div className="derechos">
+                <h2>Coquere escuela de cocina 2022 - Todos los derechos reservados</h2>
+            </div>
         </footer>
     )
 }
