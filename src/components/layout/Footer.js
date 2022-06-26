d7056b958d383a7b2de16b44baa6e5f3a1bd534f
Pablo <delatorre.pablodaniel@gmail.com>
2022-06-26 19:37:28 -0300

    footer comenzado, ingresando informacion, y dando estructura

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,30 @@
 const Footer = () => {
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer className="footer">
+            <div>
+                <h2>Coquere escuela de cocina</h2>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+            <div>
+                <h2>Nuestras instalaciones</h2>
+                <p>Direccion</p>
+                <p>Complemento direccion</p>
+            </div>
+            <div>
+                <h2>Contacto</h2>
+                <p>Teléfono</p>
+                <p>Celular</p>
+                <p>Correo</p>
+            </div>
+            <div>
+                <h2>Redes sociales</h2>
+                <a href="">Facebook</a>
+                <a href="">Instagram</a>
+            </div>
+            <div></div>
+            <div></div>
         </footer>
     )
 }
