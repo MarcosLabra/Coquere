7d77f582f27fbd4160b67b5dbe1198a8904d138c
Pablo <delatorre.pablodaniel@gmail.com>
2022-06-27 15:45:54 -0300

    componente chef funcionando con imagen titulo y texto pasados por atributos, puesto provisoriamente en pagina nosotros.js para poder visualizarlo

---
+++ src/components/layout/instalaciones.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const instalaciones = () => {
+    return (
+        <div>instalaciones</div>
+    )
+}
+
+export default instalaciones
--- src/pages/Nosotros.js
+++ src/pages/Nosotros.js
@@ -1,5 +1,6 @@
 import PortadaPaginas from "../components/layout/PortadaPaginas";
 import { HistoriaCoquere } from "../components/layout/HistoriaCoquere";
+import { Chef } from "../components/Chef";
 
 const Nosotros = () => {
     
@@ -7,6 +8,7 @@ const Nosotros = () => {
         <main className="sobreNosotros">
             <PortadaPaginas img={"/images/nosotros/sobreNosotros.jpg"} titulo={"Sobre Nosotros"}/>
             <HistoriaCoquere/>
+            <Chef img={"/images/chefs/chef1.svg"} titulo={"Julián Sandoval, Chef internacional"} texto={"Especialidad: Cocina salada"}/>
         </main>
     )
 }
