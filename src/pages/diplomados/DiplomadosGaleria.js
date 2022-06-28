42df88eb912fbb890b3ff0a27f2d45d30292841c
MarcosLabra <mlabra.dev@gmail.com>
2022-06-28 18:53:03 -0300

    modifico DiplomadosGaleria para que funcione con un map

--- src/pages/diplomados/DiplomadosGaleria.js
+++ src/pages/diplomados/DiplomadosGaleria.js
@@ -1,16 +1,37 @@
 import DiplomadosCard from "../../components/cards/DiplomadosCard"
 
+const diplomados =
+[
+    {
+        img: "/images/diplomados/diplomado1.png",
+        titulo: "Reposteria basica",
+        duracion: "4 meses"
+    },
+    {
+        img: "/images/diplomados/diplomado1.png",
+        titulo: "Cocina Salada",
+        duracion: "4 meses"
+    },
+    {
+        img: "/images/diplomados/diplomado1.png",
+        titulo: "Cocteleria",
+        duracion: "4 meses"
+    },
+    {
+        img: "/images/diplomados/diplomado1.png",
+        titulo: "Panaderia",
+        duracion: "4 meses"
+    }
+]
+
 const DiplomadosGaleria = () => {
+
     return (
         <section className="diplomadosGaleria">
 
-            <DiplomadosCard img={"/images/diplomados/diplomado1.png"} titulo={"Reposteria basica"} duracion={"4 meses"} />
-            <DiplomadosCard img={"/images/diplomados/diplomado1.png"} titulo={"Reposteria basica"} duracion={"4 meses"} />
-            <DiplomadosCard img={"/images/diplomados/diplomado1.png"} titulo={"Reposteria basica"} duracion={"4 meses"} />
-            <DiplomadosCard img={"/images/diplomados/diplomado1.png"} titulo={"Reposteria basica"} duracion={"4 meses"} />
-            <DiplomadosCard img={"/images/diplomados/diplomado1.png"} titulo={"Reposteria basica"} duracion={"4 meses"} />
-            <DiplomadosCard img={"/images/diplomados/diplomado1.png"} titulo={"Reposteria basica"} duracion={"4 meses"} />
-
+            {diplomados.map((diplomados) => {
+                return (<DiplomadosCard img={diplomados.img} titulo={diplomados.titulo} duracion={diplomados.duracion} />)
+            })}
         </section>
     )
 }
