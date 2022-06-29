8f07464d70ef039866bb454b2cfdfffad2db3c22
Pablo <delatorre.pablodaniel@gmail.com>
2022-06-29 00:20:05 -0300

    Version mobile del footer terminada

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                     <a href="">Actividades empresariales</a>
                 </div>
             </div>
-            <div className="instalaciones">
+            <div className="nuestrasInstalaciones">
                 <h2>Nuestras instalaciones</h2>
                 <div>
                     <p>Direccion</p>
@@ -24,12 +24,17 @@ const Footer = () => {
                     <p>Correo</p>
                 </div>
             </div>
-            <div className="redes">
+            <div className="redesSociales">
                 <h2>Redes sociales</h2>
                 <div>
-                    <a href="">Facebook</a>
-                    <span>-</span>
-                    <a href="">Instagram</a>
+                    <div className="redesInLine">
+                        <img src="/images/redes/redesFacebook.jpg" alt="logo facebook" />
+                        <a href="">Facebook</a>
+                    </div>
+                    <div className="redesInLine">
+                        <img src="/images/redes/redesInstagram.jpg" alt="logo instagram" />
+                        <a href="">Instagram</a>
+                    </div>
                 </div>
             </div>
             <div className="logo">
@@ -37,7 +42,7 @@ const Footer = () => {
             </div>
             <hr />
             <div className="derechos">
-                <h2>Coquere escuela de cocina 2022 - Todos los derechos reservados</h2>
+                <h2>Coquere escuela de cocina 2022 - <br></br>  Todos los derechos reservados</h2>
             </div>
         </footer>
     )
