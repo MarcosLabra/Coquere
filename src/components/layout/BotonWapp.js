1ab5551d611c9a836a357657bd852245b5f376a0
MarcosLabra <mlabra.dev@gmail.com>
2022-06-30 21:01:13 -0300

    corrijo falla en los links

--- src/components/layout/BotonWapp.js
+++ src/components/layout/BotonWapp.js
@@ -1,9 +1,11 @@
-const BotonWapp = ({texto, url}) => {
+import { Link } from "react-router-dom"
+
+const BotonWapp = ({ texto, url }) => {
   return (
-    <a href={url} className="boton" target="_blank" rel="noreferrer">
-    <img src="/images/icons/whats-app-blanco.svg" alt="" srcSet="" className="boton--imagen" />
-    <p className="boton--texto">{texto}</p>
-</a>
+    <Link to={url} className="boton" target="_blank" rel="noreferrer">
+      <img src="/images/icons/whats-app-blanco.svg" alt="" srcSet="" className="boton--imagen" />
+      <p className="boton--texto">{texto}</p>
+    </Link>
   )
 }
 
