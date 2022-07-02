26ab5d57da5e84483ec9b1b6c2b953b8cd33a41f
MarcosLabra <mlabra.dev@gmail.com>
2022-07-02 12:05:06 -0300

    hago visibles las flechas de los carrouseles en mobile

--- src/pages/talleres/TalleresCarrousel.js
+++ src/pages/talleres/TalleresCarrousel.js
@@ -1,13 +1,14 @@
 import AwesomeSlider from 'react-awesome-slider';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
-import 'react-awesome-slider/dist/styles.css';
+import AwesomeSliderStyle from "../../styles/awesomeSlider.scss"
+
 import TalleresCard from '../../components/cards/TalleresCard';
 
 
 const TalleresCarrousel = ({ contenido }) => {
     const AutoplaySlider = withAutoplay(AwesomeSlider);
     const { titulo, taller } = contenido;
-    
+
     return (
         <section className="talleresCarrousel">
             <h4>{titulo}</h4>
@@ -17,6 +18,7 @@ const TalleresCarrousel = ({ contenido }) => {
                 interval={2500}
                 bullets={false}
                 className='talleresCarrousel__slider'
+                cssModule={AwesomeSliderStyle}
             >
                 {taller.map((taller) => {
                     return (<div><TalleresCard img={taller.img} titulo={taller.titulo} duracion={taller.duracion} /></div>)
