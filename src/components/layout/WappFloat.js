9a6441b4af52966782a71a9600ad1984b011489f
MarcosLabra <mlabra.dev@gmail.com>
2022-07-04 17:19:57 -0300

    agrego wapp fixed

---
+++ src/components/layout/WappFloat.js
@@ -0,0 +1,9 @@
+const WappFloat = () => {
+  return (
+    <a href="https://wa.me/524447155980" className="wappFloat" target="_blank" rel="noreferrer">
+        <img src="images/icons/wappFloat.svg" alt="" srcset="" />
+    </a>
+  )
+}
+
+export default WappFloat
