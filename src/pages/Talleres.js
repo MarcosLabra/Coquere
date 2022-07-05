3750451ded4a93fcab9b03e529f078dd09824ced
MarcosLabra <mlabra.dev@gmail.com>
2022-07-05 20:03:59 -0300

    agrego componentes  a talleres

--- src/pages/Talleres.js
+++ src/pages/Talleres.js
@@ -4,6 +4,10 @@ import PortadaPaginas from "../components/layout/PortadaPaginas"
 import TalleresTexto from "./talleres/TalleresTexto"
 import TalleresCarrouselReposteria from "./talleres/TalleresCarrouselReposteria"
 import TalleresCarrouselReposteriaTablet from "./talleres/TalleresCarrouselReposteriaTablet"
+import TalleresCarrouselComidaSalada from "./talleres/TalleresCarrouselComidaSalada"
+import TalleresCarrouselComidaSaladaTablet from "./talleres/TalleresCarrouselComidaSaladaTablet"
+import TalleresCarrouselCocteleria from "./talleres/TalleresCarrouselCocteleria"
+import TalleresCarrouselCocteleriaTablet from "./talleres/TalleresCarrouselCocteleriaTablet"
 
 
 const Talleres = () => {
@@ -24,6 +28,8 @@ const Talleres = () => {
             <PortadaPaginas img={"/images/portadas/talleresCortosPortada.png"} titulo={"Talleres cortos"} />
             <TalleresTexto />
             {window.innerWidth < 768 ? <TalleresCarrouselReposteria /> : <TalleresCarrouselReposteriaTablet />}
+            {window.innerWidth < 768 ? <TalleresCarrouselComidaSalada /> : <TalleresCarrouselComidaSaladaTablet />}
+            {window.innerWidth < 768 ? <TalleresCarrouselCocteleria /> : <TalleresCarrouselCocteleriaTablet />}
 
             <BotonWapp texto={"¡Quiero inscribirme!"} url={"https://alvo.chat/4oD"} />
         </main>
--- src/pages/talleres/TalleresCarrouselComidaSaladaTablet.js
+++ src/pages/talleres/TalleresCarrouselComidaSaladaTablet.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import AwesomeSlider from 'react-awesome-slider';
+import TalleresCard from '../../components/cards/TalleresCard';
 
 const taller = [
     {
